08e6bc20e740944172e186d4a31678f04f39e4f3
anuragrathor <45478290+anuragrathor@users.noreply.github.com>
2023-05-23 15:49:10 +0530

    test

--- videos/ffmpeg/ffmpeg.js
+++ videos/ffmpeg/ffmpeg.js
@@ -279,4 +279,324 @@ router.get('/ffmpeg/stichimageinvideo', async (req, res )=> {
 })
 
 
+
+//Add Image in video as a watermark
+router.get('/ffmpeg/addwatermark', async (req, res )=> {
+
+  const filePath = 'uploads/tes.mp4';
+  const imagePath = 'uploads/test.jpg';
+
+
+  const imagePath1 = 'uploads/overlay-1.jpg';
+  const imagePath2 = 'uploads/overlay-2.jpg';
+  const imagePath3 = 'uploads/overlay-3.jpg';
+  
+
+  // Run FFmpeg
+  ffmpeg()
+  
+    // Input file
+    .input(filePath)
+    .input(imagePath)   //Activate if we need single image 
+    // .input(imagePath1)
+    // .input(imagePath2)
+    // .input(imagePath3)
+
+    .complexFilter([
+        //"overlay=main_w-overlay_w-10:main_h-overlay_h-10",  //To place the overlay in the right bottom corner
+        //"[0][1]overlay=x=0:y=0",  //To place the overlay in the top-left corner:
+        //"[0][1]overlay=x=0:y=(main_h-overlay_h)",  //To place the overlay in the bottom-left corner:
+        //"[0][1]overlay=x=(main_w-overlay_w):y=(main_h-overlay_h)",  //To place the overlay in the bottom-right corner:
+        //"[0][1]overlay=x=0:y=0",  //To place the overlay in the top-left corner:
+        //"[0][1]overlay=x=(main_w-overlay_w):y=0",  //To place the overlay in the top-right corner:
+        //"[0][1]overlay=x=(main_w-overlay_w)/2:y=(main_h-overlay_h)/2",  //To place the overlay in the center:
+
+        //"[0][1]overlay=enable='between(t,0,4)':x=0:y=0[out];[out][2]overlay=enable='between(t,4,8)':x=0:y=0[out];[out][3]overlay=enable='between(t,8,12)':x=0:y=0",  //Overlaying a sequence of images
+        "overlay='if(gte(t,1),-w+(t-1)*200,NAN)':(main_h-overlay_h)/2",  // Image Animation 
+      ])
+    //.videoCodec('libx264')
+    .outputOptions('-pix_fmt yuv420p')
+
+  
+    // Output file
+    .saveToFile('uploads/'+'audio.mp4')
+  
+    // Log the percentage of work completed
+    .on('progress', (progress) => {
+      if (progress.percent) {
+        console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+      }
+    })
+  
+    // The callback that is run when FFmpeg is finished
+    .on('end', () => {
+      console.log('FFmpeg has finished.');
+      return res.json({
+        status: true,
+        message: 'Audio Extract From Video Successfully',
+        data: 'Fmpeg has finished'
+      })
+    })
+  
+    // The callback that is run when FFmpeg encountered an error
+    .on('error', (error) => {
+      console.error(error);
+      return res.json({
+        status: false,
+        message: 'Something Error Found',
+        data: error
+      })
+    });
+  
+  
+  
+  
+  })
+
+
+
+  //Rotate Video
+router.get('/ffmpeg/rotatevideo', async (req, res )=> {
+
+  const filePath = 'uploads/tes.mp4';
+  
+  // Run FFmpeg
+  ffmpeg()
+  
+    // Input file
+    .input(filePath) 
+
+  
+    .withVideoFilter('transpose=2')
+
+    // 0 = 90CounterCLockwise and Vertical Flip (default)
+    // 1 = 90Clockwise
+    // 2 = 90CounterClockwise
+    // 3 = 90Clockwise and Vertical Flip
+  
+    // Output file
+    .saveToFile('uploads/'+'audio.mp4')
+  
+    // Log the percentage of work completed
+    .on('progress', (progress) => {
+      if (progress.percent) {
+        console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+      }
+    })
+  
+    // The callback that is run when FFmpeg is finished
+    .on('end', () => {
+      console.log('FFmpeg has finished.');
+      return res.json({
+        status: true,
+        message: 'Audio Extract From Video Successfully',
+        data: 'Fmpeg has finished'
+      })
+    })
+  
+    // The callback that is run when FFmpeg encountered an error
+    .on('error', (error) => {
+      console.error(error);
+      return res.json({
+        status: false,
+        message: 'Something Error Found',
+        data: error
+      })
+    });
+  
+  
+  
+  
+})
+
+
+// To split OR Cut video from some time to some and make new video
+  router.get('/ffmpeg/cutvideo', async (req, res )=> {
+
+    const filePath = 'uploads/test.mp4';
+  
+    // Run FFmpeg
+    ffmpeg()
+    
+      // Input file
+      .input(filePath) 
+    
+      .setStartTime('00:00:10')
+
+      .setDuration(10)
+    
+      // Output file
+      .saveToFile('uploads/'+'audio1.mp4')
+    
+      // Log the percentage of work completed
+      .on('progress', (progress) => {
+        if (progress.percent) {
+          console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+        }
+      })
+    
+      // The callback that is run when FFmpeg is finished
+      .on('end', () => {
+        console.log('FFmpeg has finished.');
+        return res.json({
+          status: true,
+          message: 'Audio Extract From Video Successfully',
+          data: 'Fmpeg has finished'
+        })
+      })
+    
+      // The callback that is run when FFmpeg encountered an error
+      .on('error', (error) => {
+        console.error(error);
+        return res.json({
+          status: false,
+          message: 'Something Error Found',
+          data: error
+        })
+      });
+    
+    
+  })
+
+
+  // To convert Extention of your Video File 
+  router.get('/ffmpeg/convertExtention', async (req, res )=> {
+
+    const filePath = 'uploads/tes.mp4';
+  
+    // Run FFmpeg
+    ffmpeg()
+    
+      // Input file
+      .input(filePath) 
+    
+      // Output file
+      .saveToFile('uploads/'+'audio2.webm')
+    
+      // Log the percentage of work completed
+      .on('progress', (progress) => {
+        if (progress.percent) {
+          console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+        }
+      })
+    
+      // The callback that is run when FFmpeg is finished
+      .on('end', () => {
+        console.log('FFmpeg has finished.');
+        return res.json({
+          status: true,
+          message: 'Audio Extract From Video Successfully',
+          data: 'Fmpeg has finished'
+        })
+      })
+    
+      // The callback that is run when FFmpeg encountered an error
+      .on('error', (error) => {
+        console.error(error);
+        return res.json({
+          status: false,
+          message: 'Something Error Found',
+          data: error
+        })
+      });
+    
+    
+  })
+
+
+  // To Merge Two or more Video Files in one 
+  router.get('/ffmpeg/mergefiles', async (req, res )=> {
+
+    const filePath = 'uploads/tes.mp4';
+    const filePath2 = 'uploads/test.mp4';
+  
+    // Run FFmpeg
+    ffmpeg()
+    
+      // Input file
+      .input(filePath) 
+      .input(filePath2) 
+    //.input(thirdFile)
+    //.input(...)
+    
+      // Output file
+      .mergeToFile('uploads/'+'audio3.mp4')
+    
+      // Log the percentage of work completed
+      .on('progress', (progress) => {
+        if (progress.percent) {
+          console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+        }
+      })
+    
+      // The callback that is run when FFmpeg is finished
+      .on('end', () => {
+        console.log('FFmpeg has finished.');
+        return res.json({
+          status: true,
+          message: 'Audio Extract From Video Successfully',
+          data: 'Fmpeg has finished'
+        })
+      })
+    
+      // The callback that is run when FFmpeg encountered an error
+      .on('error', (error) => {
+        console.error(error);
+        return res.json({
+          status: false,
+          message: 'Something Error Found',
+          data: error
+        })
+      });
+    
+    
+  })
+
+
+  // Take screenshot from Video
+  router.get('/ffmpeg/screenshot', async (req, res )=> {
+
+    const filePath = 'uploads/tes.mp4';
+  
+    // Run FFmpeg
+    ffmpeg()
+    
+      // Input file
+      .input(filePath) 
+    
+       // take 2 screenshots at predefined timemarks and size
+      .takeScreenshots({ count: 2, timemarks: [ '00:00:02.000', '6' ], size: '150x100' }, 'uploads/thumbnail')
+    
+      // Log the percentage of work completed
+      .on('progress', (progress) => {
+        if (progress.percent) {
+          console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+        }
+      })
+    
+      // The callback that is run when FFmpeg is finished
+      .on('end', () => {
+        console.log('FFmpeg has finished.');
+        return res.json({
+          status: true,
+          message: 'Audio Extract From Video Successfully',
+          data: 'Fmpeg has finished'
+        })
+      })
+    
+      // The callback that is run when FFmpeg encountered an error
+      .on('error', (error) => {
+        console.error(error);
+        return res.json({
+          status: false,
+          message: 'Something Error Found',
+          data: error
+        })
+      });
+    
+    
+  })
+
+
+
 module.exports = router;
