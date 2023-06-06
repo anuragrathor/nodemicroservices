69501dc05ee422bae551fdcb5d2204e526fd734e
anuragrathor <45478290+anuragrathor@users.noreply.github.com>
2023-06-06 14:42:34 +0530

    sdsd

--- _utility/logger/morgan-logger.js
+++ _utility/logger/morgan-logger.js
@@ -15,13 +15,37 @@ const path = require('path');
 // var accessLogStream = fs.createWriteStream(path.join(__dirname, 'morgan-log.log'), { flags: 'a' })  //Create file in same directory
 var accessLogStream = fs.createWriteStream(path.join('logs/morgan-log.log'), { flags: 'a' })
 
-const morgan_var = morgan(':method :url :status :http-version :response-time ', { stream: accessLogStream })
+const morgan_var = morgan(':method :url :status :http-version :res[content-length] :response-time ms', { stream: accessLogStream })
 
-//const morgan_var = morgan('combined', { stream: accessLogStream })
+//:method :url :status :http-version :res[content-length] - :response-time ms
+
+// const morgan_var = morgan('combined', { stream: accessLogStream })
 // const morgan_var = morgan('tiny', { stream: accessLogStream })
 // const morgan_var = morgan('short', { stream: accessLogStream })  // It also log API execution time 
 // const morgan_var = morgan('dev', { stream: accessLogStream })    // It log API execution time 
 // const morgan_var = morgan('common', { stream: accessLogStream })
+
+
+// EXAMPLE: only log error responses
+// morgan('combined', {
+//     skip: function (req, res) { return res.statusCode < 400 }
+//   })
+
+  
+
+//Using a Custom Format Function
+// morgan(function (tokens, req, res) {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, 'content-length'), '-',
+//       tokens['response-time'](req, res), 'ms'
+//     ].join(' ')
+//   })
+  
+
+
 
 
 module.exports = morgan_var;
