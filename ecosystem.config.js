87a50068ee594974b23ab60a8fb6bd040643ce40
anuragrathor <45478290+anuragrathor@users.noreply.github.com>
2023-09-08 12:42:46 +0530

    asdad

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [
+    {
+    name   : "auth",
+    script : "./authentication/index.js"
+  },
+  {
+    name   : "product",
+    script : "./product/index.js"
+  },
+  {
+    name   : "video",
+    script : "./video/index.js"
+  }
+]
+}
